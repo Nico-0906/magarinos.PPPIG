87d23d58b8ccf1b34b1a4189c1ebe15b8a17ad19
nico-0906 <nico1844@gmail.com>
2020-05-18 19:43:51 -0300

    ultimo ejercicio completado

--- magarinosPPPIG/main.c
+++ magarinosPPPIG/main.c
@@ -1,11 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+#define TAMNOTE 5
+
+typedef struct{
+    int id;
+    char procesador[20];
+    char marca[20];
+    float precio;
+
+}eNotebook;
+
+void ordenarMarcaPrecio(eNotebook notebooks[], int tam);
 float aplicarDescuento(float precio);
 int contarCaracteres(char cadena[], char caracter);
 
 int main()
 {
+    eNotebook notebooks[TAMNOTE] = {{100, "Intel", "Dell", 70500.50}, {101, "AMD", "Bangho", 50366.30}, {102, "Intel", "Comodore", 25500.10}, {103, "AMD", "Dell", 25500.20}, {104, "Intel", "Bangho", 30000.50}};
+
+    printf("*****Antes de ordenar******\n\n");
+    printf("ID        PROCESADOR      MARCA      PRECIO\n");
+    for(int i = 0 ; i < TAMNOTE; i++){
+        printf("%d    %10s     %10s     %8.2f\n", notebooks[i].id, notebooks[i].procesador, notebooks[i].marca, notebooks[i].precio);
+    }
+
+    ordenarMarcaPrecio(notebooks, TAMNOTE);
+
+    printf("\n*****Despues de ordenar*****\n\n");
+    printf("ID        PROCESADOR      MARCA      PRECIO\n");
+    for(int i = 0 ; i < TAMNOTE; i++){
+        printf("%d    %10s     %10s     %8.2f\n", notebooks[i].id, notebooks[i].procesador, notebooks[i].marca, notebooks[i].precio);
+    }
+
     float precioSinDescuento = 100.10;
     float precioConDescuento;
 
@@ -44,3 +72,29 @@ int contarCaracteres(char cadena[], char caracter){
     return retorno;
 }
 
+void ordenarMarcaPrecio(eNotebook notebooks[], int tam){
+    eNotebook auxNotebook;
+
+        for(int i = 0; i < tam - 1 ; i++){
+            for(int j = i + 1; j < tam ; j++){
+                if(strcmp(notebooks[i].marca, notebooks[j].marca) > 0 ){
+                    auxNotebook = notebooks[i];
+                    notebooks[i] = notebooks[j];
+                    notebooks[j] = auxNotebook;
+
+
+                }
+            }
+        }
+
+        for(int i = 0; i < tam - 1 ; i++){
+            for(int j = i + 1; j < tam ; j++){
+                if(strcmp(notebooks[i].marca, notebooks[j].marca) == 0 && notebooks[i].precio < notebooks[j].precio){
+                    auxNotebook = notebooks[i];
+                    notebooks[i] = notebooks[j];
+                    notebooks[j] = auxNotebook;
+                }
+            }
+        }
+
+}
