b89e9ffff07065eae3031541f2dc26362f2ff65c
nico-0906 <nico1844@gmail.com>
2020-05-18 19:14:31 -0300

    segundo ejercicio resuelto

--- magarinosPPPIG/main.c
+++ magarinosPPPIG/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 float aplicarDescuento(float precio);
+int contarCaracteres(char cadena[], char caracter);
 
 int main()
 {
@@ -18,9 +19,28 @@ int main()
 float aplicarDescuento(float precio){
     float retorno;
     float descuento;
+    char cadenaContar[20] = {"Buenas tardes"};
+    int cantidadDeA;
+
+    cantidadDeA = contarCaracteres(cadenaContar, 'a');
+
+    printf("La cantidad de A en la cadena es %d \n", cantidadDeA);
 
     descuento = (5 * precio) / 100;
     retorno = precio - descuento;
 
     return retorno;
 }
+
+int contarCaracteres(char cadena[], char caracter){
+    int retorno = 0;
+
+    for(int i = 0; cadena[i] != '\0' ; i++){
+        if(cadena[i] == caracter){
+            retorno++;
+        }
+    }
+
+    return retorno;
+}
+
